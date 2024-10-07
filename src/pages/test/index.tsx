65642384586f5bdd38fda719334c0f028020b25c
Choi Young Sun <sunn7931@naver.com>
2024-10-07 21:30:41 +0900

    ZERO-26 [style]: 인라인 스타일 테일윈드로 수정

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,18 +1,12 @@
 export default function Test() {
   return (
-    <div
-      className="grid grid-cols-3 gap-4 p-4"
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr)',
-        gap: '16px',
-        padding: '16px',
-      }}
-    >
+    <div className="grid grid-cols-3 gap-4 p-4">
       <button className="bg-red-100 text-red-500" type="button">
         버튼1
       </button>
-      <button type="button">버튼2</button>
+      <button className="bg-blue-100 text-blue-500" type="button">
+        버튼2
+      </button>
       <button type="button">버튼3</button>
       <button type="button">버튼4</button>
       <button type="button">버튼5</button>
