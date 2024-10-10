5a3a49589f4ae7b1fbbd638c5830e9593ac7f126
jangyonghan <wkddydgks1@gmail.com>
2024-10-11 01:45:27 +0900

    ZERO-47 [feat] : 마이히스토리 페이지 수정

---
+++ src/components/myhistory/MyTask.tsx
@@ -0,0 +1,16 @@
+import CheckBoxIconActiveIcon from 'public/icons/checkbox_active.svg';
+import KebabIcon from 'public/icons/kebab_small.svg';
+
+export default function MyTask() {
+  return (
+    <div className="flex flex-col gap-4  ">
+      <div className=" relative flex min-w-[270px] items-center justify-between break-all rounded-md bg-background-secondary px-3.5 py-2.5 text-md-regular">
+        <div className="flex items-center gap-1.5  ">
+          <CheckBoxIconActiveIcon />
+          <span className="line-through">법인 설립 안내 드리기</span>
+        </div>
+        <KebabIcon />
+      </div>
+    </div>
+  );
+}
