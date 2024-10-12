860a57744343110e9347feed92132786657c733f
Choi Young Sun <sunn7931@naver.com>
2024-10-13 03:56:37 +0900

    ZERO-82 [Feat]: 리포트 섹션 퍼블리싱

--- src/components/team/Report.tsx
+++ src/components/team/Report.tsx
@@ -1,36 +1,55 @@
+import Image from 'next/image';
 import CircleGraph from './CircleGraph';
 
 export default function Report() {
   return (
-    <div>
-      <CircleGraph
-        backgroundColor="#334155"
-        gradientColorStart="#10B981"
-        gradientColorEnd="#A3E635"
-        radius={54}
-        percentage={40}
-        strokeWidth={20}
-      />
-      <CircleGraph
-        backgroundColor="#ffffff"
-        gradientColorStart="#FDFF27"
-        gradientColorEnd="#ff8c00"
-        radius={30}
-        percentage={40}
-        strokeWidth={20}
-      />
-      <p>오늘의 진행상황</p>
-      <p>25%</p>
-      <div>
-        <p>오늘의 할 일</p>
-        <p>20개</p>
-        <p>아이콘</p>
+    <section>
+      <p className="my-[20px] text-lg-medium">리포트</p>
+      <div className="flex h-[217px] items-center justify-between rounded-[12px] bg-background-secondary px-[24px]">
+        <div className="flex items-center gap-[20px]">
+          <CircleGraph
+            backgroundColor="#334155"
+            gradientColorStart="#10B981"
+            gradientColorEnd="#A3E635"
+            radius={60}
+            percentage={25}
+            strokeWidth={30}
+          />
+          <div className="text-md-medium">
+            <p>오늘의</p>
+            <p>진행상황</p>
+            <p className="bg-brand-gradient bg-clip-text text-4xl text-transparent">
+              25%
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="flex h-[76.5px] w-[400px] items-center justify-between rounded-[12px] bg-background-tertiary px-[16px]">
+            <div className="flex flex-col gap-[4px]">
+              <p className="text-xs-medium text-text-secondary">오늘의 할 일</p>
+              <p className="text-2xl-bold text-brand-tertiary">20개</p>
+            </div>
+            <Image
+              width={50}
+              height={30}
+              src="/images/todo.png"
+              alt="얼굴 아이콘"
+            />
+          </div>
+          <div className="mt-[8px] flex h-[76.5px] w-[400px] items-center justify-between rounded-[12px] bg-background-tertiary px-[16px]">
+            <div className="flex flex-col gap-[4px]">
+              <p className="text-xs-medium text-text-secondary">한 일</p>
+              <p className="text-2xl-bold text-brand-tertiary">5개</p>
+            </div>
+            <Image
+              width={50}
+              height={30}
+              src="/images/done_signs.png"
+              alt="한 일 아이콘"
+            />
+          </div>
+        </div>
       </div>
-      <div>
-        <p>한 일</p>
-        <p>5개</p>
-        <p>아이콘</p>
-      </div>
-    </div>
+    </section>
   );
 }
