7d1fe56c9b4c06a74f2eafc7c7d9093ad1046894
hyeoksuJ <hyeoksu459@naver.com>
2024-10-13 19:58:50 +0900

    ZERO-49 [Feat]: 회원가입, 로그인 비밀번호 토글 기능

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,8 +1,17 @@
 import Button from '@components/@shared/Button';
 import { IconInput, Input } from '@components/@shared/Input';
 import NonVisibleIcon from '@icons/visibility_off.svg';
+import VisibleIcon from '@icons/visibility_on.svg';
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function SignInForm() {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+
   return (
     <div className="flex flex-col items-center gap-[24px]">
       <div className="mt-[24px] flex w-[343px] flex-col gap-[40px] md:mt-[100px] md:w-[460px]">
@@ -15,18 +24,30 @@ export default function SignInForm() {
             <IconInput
               label="비밀번호"
               placeholder="비밀번호를 입력해주세요."
-              actionIcon={<NonVisibleIcon />}
+              inputProps={{ type: isPasswordVisible ? 'text' : 'password' }}
+              actionIcon={
+                isPasswordVisible ? (
+                  <VisibleIcon onClick={togglePasswordVisibility} />
+                ) : (
+                  <NonVisibleIcon onClick={togglePasswordVisibility} />
+                )
+              }
             />
           </form>
-          <span className="text-right text-[14px] font-medium leading-[24px] text-interaction-focus underline md:text-[16px]">
+          <Link
+            href="/#"
+            className="text-right text-[14px] font-medium leading-[24px] text-interaction-focus underline md:text-[16px]"
+          >
             비밀번호를 잊으셨나요?
-          </span>
+          </Link>
         </div>
         <Button size="full">로그인</Button>
       </div>
       <div className="flex items-center gap-[12px] text-md-medium md:text-lg-medium">
         <span className="text-text-primary">아직 계정이 없으신가요?</span>
-        <span className="text-interaction-focus underline">가입하기</span>
+        <Link href="/signup" className="text-interaction-focus underline">
+          가입하기
+        </Link>
       </div>
     </div>
   );
--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,8 +1,22 @@
 import Button from '@components/@shared/Button';
 import { IconInput, Input } from '@components/@shared/Input';
 import NonVisibleIcon from '@icons/visibility_off.svg';
+import VisibleIcon from '@icons/visibility_on.svg';
+import { useState } from 'react';
 
 export default function SignUpForm() {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] =
+    useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setIsConfirmPasswordVisible((prev) => !prev);
+  };
+
   return (
     <div className="mt-[24px] flex w-[343px] flex-col gap-[40px] md:mt-[100px] md:w-[460px]">
       <form className="flex w-full flex-col gap-[24px]">
@@ -14,12 +28,26 @@ export default function SignUpForm() {
         <IconInput
           label="비밀번호"
           placeholder="비밀번호를 입력해주세요."
-          actionIcon={<NonVisibleIcon />}
+          inputProps={{ type: isPasswordVisible ? 'text' : 'password' }}
+          actionIcon={
+            isPasswordVisible ? (
+              <VisibleIcon onClick={togglePasswordVisibility} />
+            ) : (
+              <NonVisibleIcon onClick={togglePasswordVisibility} />
+            )
+          }
         />
         <IconInput
           label="비밀번호 확인"
           placeholder="비밀번호를 다시 한 번 입력해주세요."
-          actionIcon={<NonVisibleIcon />}
+          inputProps={{ type: isConfirmPasswordVisible ? 'text' : 'password' }}
+          actionIcon={
+            isConfirmPasswordVisible ? (
+              <VisibleIcon onClick={toggleConfirmPasswordVisibility} />
+            ) : (
+              <NonVisibleIcon onClick={toggleConfirmPasswordVisibility} />
+            )
+          }
         />
       </form>
       <Button size="full">회원가입</Button>
