be9c4c1afb9bc7b840104539b0b12b12ed8bf420
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-10-17 17:05:03 +0900

    ZERO-44 [Refactor]: date 값 Context 관리

---
+++ src/contexts/DateContext.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+import { createContext, useContext, useState, ReactNode } from 'react';
+import dayjs from 'dayjs';
+
+interface DateContextProps {
+  date: dayjs.Dayjs;
+  setDate: (date: dayjs.Dayjs) => void;
+  today: dayjs.Dayjs;
+}
+
+const DateContext = createContext<DateContextProps | undefined>(undefined);
+
+export function DateProvider({ children }: { children: ReactNode }) {
+  const today = dayjs(); // 오늘 날짜
+  const [date, setDate] = useState<dayjs.Dayjs>(today); // 현재 날짜 상태
+
+  return (
+    <DateContext.Provider value={{ date, setDate, today }}>
+      {children}
+    </DateContext.Provider>
+  );
+}
+
+export const useDate = (): DateContextProps => {
+  const context = useContext(DateContext);
+  if (!context) {
+    throw new Error('DateContext error');
+  }
+  return context;
+};
