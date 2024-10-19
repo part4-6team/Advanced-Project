c0fa62eb784a67c66f917e2a1f0e8578d165c2aa
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-10-19 16:20:35 +0900

    ZERO-45 [Design]: 캘린더 모바일 디자인 수정, 모달 팝업

--- src/components/tasks/UI/Calender.tsx
+++ src/components/tasks/UI/Calender.tsx
@@ -2,7 +2,10 @@ import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import dayjs, { Dayjs } from 'dayjs';
 
+import { Modal } from '@components/@shared/Modal';
+import Button from '@components/@shared/Button';
 import { useDate } from '@/src/contexts/DateContext';
+import useViewportSize from '@hooks/useViewportSize';
 import CalenderIcon from '@icons/calendar.svg';
 import CalenderArrowLeft from '@icons/calender_arrow_left.svg';
 import CalenderArrowRight from '@icons/calender_arrow_right.svg';
@@ -21,6 +24,7 @@ export default function Calender() {
   const [selectedDate, setSelectedDate] = useState<Dayjs>(dayjs(contextDate));
   const [displayedMonth, setDisplayedMonth] = useState(dayjs(contextDate));
   const [isOpen, setIsOpen] = useState(false);
+  const { isMobile } = useViewportSize();
 
   const handleDateChange = (newDate: Date | null) => {
     if (newDate) {
@@ -64,50 +68,78 @@ export default function Calender() {
     );
   };
 
+  // 공통 DatePicker 컴포넌트
+  const renderDatePicker = () => (
+    <DatePicker
+      inline
+      dateFormat="yyyy년 MM월 dd일"
+      selected={selectedDate.toDate()}
+      renderCustomHeader={renderCustomHeader}
+      calendarClassName={styles.calenderWrapper}
+      dayClassName={(date) => {
+        // 날짜 확인
+        const day = dayjs(date);
+        const isDisplayedMonth = day.isSame(displayedMonth, 'month');
+        const isToday = day.isSame(today, 'day');
+        const isSelected =
+          day.isSame(selectedDate, 'month') &&
+          day.date() === selectedDate.date();
+
+        if (!isDisplayedMonth) {
+          return styles.disabledDay;
+        }
+        if (isToday) {
+          return styles.today;
+        }
+        if (isSelected) {
+          return styles.selectedDay;
+        }
+        return styles.day;
+      }}
+      filterDate={(date) => dayjs(date).isSame(displayedMonth, 'month')}
+      onChange={handleDateChange}
+    />
+  );
+
   return (
     <>
       <button
         type="button"
         className="relative h-6 rounded-full bg-background-secondary p-[6px]"
         onClick={() => {
-          setIsOpen(!isOpen);
-          if (!isOpen) {
-            setDisplayedMonth(dayjs(contextDate));
-          }
+          setIsOpen(true);
         }}
       >
         <CalenderIcon />
       </button>
-      {isOpen && (
-        <DatePicker
-          inline
-          dateFormat="yyyy년 MM월 dd일"
-          selected={selectedDate.toDate()}
-          renderCustomHeader={renderCustomHeader}
-          calendarClassName={styles.calenderWrapper}
-          dayClassName={(date) => {
-            const day = dayjs(date);
-            // 날짜 확인
-            const isDisplayedMonth = day.isSame(displayedMonth, 'month');
-            const isToday = day.isSame(today, 'day');
-            const isSelected =
-              day.isSame(selectedDate, 'month') &&
-              day.date() === selectedDate.date();
 
-            if (!isDisplayedMonth) {
-              return styles.disabledDay;
-            }
-            if (isToday) {
-              return styles.today;
-            }
-            if (isSelected) {
-              return styles.selectedDay;
-            }
-            return styles.day;
-          }}
-          filterDate={(date) => dayjs(date).isSame(displayedMonth, 'month')}
-          onChange={handleDateChange}
-        />
+      {/* 모바일 모달 */}
+      {isMobile ? (
+        <Modal
+          isOpen={isOpen}
+          onClose={() => setIsOpen(false)}
+          className="relative flex flex-1 justify-center"
+        >
+          <Modal.Wrapper className="flex flex-col">
+            <Modal.Content> {renderDatePicker()} </Modal.Content>
+            <Modal.Footer className="mr-1 flex justify-end">
+              <Button
+                className="bg-slate-400 "
+                shape="square"
+                fontColor="white"
+                fontSize="14"
+                width={60}
+                height={30}
+                border="gray"
+                onClick={() => setIsOpen(false)}
+              >
+                닫기
+              </Button>
+            </Modal.Footer>
+          </Modal.Wrapper>
+        </Modal>
+      ) : (
+        renderDatePicker()
       )}
     </>
   );
