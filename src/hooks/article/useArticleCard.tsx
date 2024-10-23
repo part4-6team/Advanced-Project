8b2c28e7bb0e62bcae06afb0117b75ff9265a207
jangyonghan <wkddydgks1@gmail.com>
2024-10-23 17:37:04 +0900

    ZERO-109 [feat] : 무한스크롤 적용

--- src/hooks/article/useArticleCard.tsx
+++ src/hooks/article/useArticleCard.tsx
@@ -1,6 +1,6 @@
 import { fetchBestCard } from '@/src/api/article/articlecardAPI';
 import { list } from '@/src/types/article/CardType';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
 export const useCard = (
   page: number,
@@ -9,8 +9,28 @@ export const useCard = (
   keyword: string
 ) => {
   return useQuery<list[]>({
-    queryKey: ['card', page, pageSize, orderBy, keyword],
+    queryKey: ['bestcard', page, pageSize, orderBy, keyword],
     queryFn: () =>
       fetchBestCard({ queryKey: [page, pageSize, orderBy, keyword] }),
+  });
+};
+
+// 무한 스크롤 관련 hook
+export const useCards = (
+  pageSize: number,
+  orderBy: string,
+  keyword: string
+) => {
+  return useInfiniteQuery({
+    queryKey: ['articleCard', pageSize, orderBy, keyword],
+    queryFn: ({ pageParam = 1 }) =>
+      fetchBestCard({ queryKey: [pageParam, pageSize, orderBy, keyword] }),
+    getNextPageParam: (lastPage, allPage) => {
+      if (lastPage.length < pageSize) {
+        return undefined;
+      }
+      return allPage.length + 1;
+    },
+    initialPageParam: 1,
   });
 };
