a2cf7e9ae77eb311355d70687652e9c25e94a362
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-10-28 02:35:25 +0900

    ZERO-76 [Design]: 캘린더 포지션 오류 수정

--- src/components/tasks/UI/CalenderButton.tsx
+++ src/components/tasks/UI/CalenderButton.tsx
@@ -6,17 +6,21 @@ export default function CalenderButton() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
+    <div className="relative">
       <button
         type="button"
-        className="relative h-6 rounded-full bg-background-secondary p-[6px]"
+        className="h-6 rounded-full bg-background-secondary p-[6px]"
         onClick={() => {
           setIsOpen((prev) => !prev);
         }}
       >
         <CalenderIcon />
       </button>
-      <Calender isOpen={isOpen} onClose={() => setIsOpen(false)} />
-    </>
+      {isOpen && (
+        <div className="absolute z-10 mt-1">
+          <Calender isOpen={isOpen} onClose={() => setIsOpen(false)} />
+        </div>
+      )}
+    </div>
   );
 }
