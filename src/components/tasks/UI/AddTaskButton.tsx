1109656cc72214a135f8e2d520bb2150dcf790dd
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-10-29 02:05:55 +0900

    ZERO-76 [Feat]: 사이드바 상태에 따라 할 일 추가 버튼 조건부 렌더링

--- src/components/tasks/UI/AddTaskButton.tsx
+++ src/components/tasks/UI/AddTaskButton.tsx
@@ -1,20 +1,24 @@
 import { useModal } from '@hooks/useModal';
+import { useTaskListStore } from '@/src/stores/taskListStore';
 import Button from '@components/@shared/Button';
 import AddTaskModal from './AddTaskModal';
 
 export default function AddTaskButton() {
   const { isOpen, onOpen, onClose } = useModal();
+  const { isSidebarOpen } = useTaskListStore();
 
   return (
     <>
-      <Button
-        bgColor="green"
-        shape="round"
-        className="fixed bottom-8 right-8 xl:bottom-12"
-        onClick={onOpen}
-      >
-        + 할 일 추가
-      </Button>
+      {!isSidebarOpen && (
+        <Button
+          bgColor="green"
+          shape="round"
+          className="fixed bottom-8 right-8 xl:bottom-12"
+          onClick={onOpen}
+        >
+          + 할 일 추가
+        </Button>
+      )}
 
       {isOpen && <AddTaskModal isOpen={isOpen} onClose={onClose} />}
     </>
