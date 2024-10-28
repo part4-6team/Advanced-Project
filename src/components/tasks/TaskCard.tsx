7bf054e323ae97eed6ce95b3c94274008f377c07
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-10-29 02:04:47 +0900

    ZERO-76 [Feat]: 드롭다운과 모달 연결 & 체크박스 로직 추가

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,24 +1,28 @@
+import { useEffect } from 'react';
 import CalenderIcon from '@icons/calendar_large.svg';
 import CommentIcon from '@icons/comment.svg';
 import RepeatIcon from '@icons/repeat.svg';
-import type { TaskDto } from '@/src/types/tasks/TaskListDto';
-import { useTaskListContext } from '@/src/contexts/TaskListContext';
+import type { TaskDto } from '@/src/types/tasks/taskDto';
 
 import { useModal } from '@hooks/useModal';
+import { useTaskListStore } from '@/src/stores/taskListStore';
 import useDropdownModals from '@hooks/useDropdownModals';
 import Image from 'next/image';
 
 import EditDropdown, { editOption } from '@components/team/EditDropdown';
 import EditTaskModal from './UI/EditTaskModal';
 import DeleteTaskModal from './UI/DeleteTaskModal';
+import { TaskSideBar } from './TaskSideBar';
 import CheckBox from './CheckBox';
 
 interface TaskCardProps {
   task: TaskDto;
+  key: number;
 }
 
 export default function TaskCard({ task }: TaskCardProps) {
-  const { updateTask } = useTaskListContext();
+  const { setCurrentTaskId, isSidebarOpen, setSidebarOpen } =
+    useTaskListStore();
 
   // 각 모달의 상태 독립적으로 관리
   const editModal = useModal();
@@ -30,15 +34,25 @@ export default function TaskCard({ task }: TaskCardProps) {
     deleteModal,
   ]);
 
+  // 드롭다운 옵션 선택 시 taskId 설정
+  const handleDropdownSelection = (option: any) => {
+    handleOptionSelect(option);
+    setCurrentTaskId(task.id);
+  };
+
   return (
     <li className="flex flex-col gap-[10px] rounded-lg bg-background-secondary px-[14px] py-3 text-text-default">
       <div className="flex">
         <div className="flex gap-2">
-          <CheckBox
-            doneAt={task.doneAt}
-            onChange={(newDoneAt) => updateTask(task.id, newDoneAt)}
+          <CheckBox taskId={task.id} doneAt={task.doneAt} />
+          <button type="button" onClick={() => setSidebarOpen(!isSidebarOpen)}>
+            <h1 className="text-text-primary">{task.name}</h1>
+          </button>
+          <TaskSideBar
+            isOpen={isSidebarOpen}
+            onClose={() => setSidebarOpen(false)}
+            taskId={task.id}
           />
-          <h1 className="text-text-primary">{task.name}</h1>
         </div>
         <div className="flex flex-grow justify-end gap-2 md:ml-2 md:justify-between">
           <div className="flex items-center gap-[2px]">
@@ -55,17 +69,22 @@ export default function TaskCard({ task }: TaskCardProps) {
                 className="h-3"
               />
             }
-            onSelect={handleOptionSelect}
+            onSelect={handleDropdownSelection}
           />
-          <EditTaskModal
-            isOpen={editModal.isOpen}
-            onClose={editModal.onClose}
-          />
-          <DeleteTaskModal
-            isOpen={deleteModal.isOpen}
-            onClose={deleteModal.onClose}
-            taskName={task.name}
-          />
+          {editModal.isOpen && (
+            <EditTaskModal
+              isOpen={editModal.isOpen}
+              onClose={editModal.onClose}
+              done={task.doneAt !== null}
+            />
+          )}
+          {deleteModal.isOpen && (
+            <DeleteTaskModal
+              isOpen={deleteModal.isOpen}
+              onClose={deleteModal.onClose}
+              taskName={task.name}
+            />
+          )}
         </div>
       </div>
       <div className="flex items-center gap-[10px] text-xs-regular">
