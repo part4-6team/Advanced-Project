e150d942d369260693f0294744a32d10dd4daaa7
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-10-28 23:48:29 +0900

    ZERO-76 [Feat]: 날짜 값을 한국 시간 기준 ISO 문자열로 변환하는 함수 추가

--- src/contexts/DateContext.tsx
+++ src/contexts/DateContext.tsx
@@ -1,20 +1,26 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import { createContext, useContext, useState, ReactNode } from 'react';
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import 'dayjs/locale/ko';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 interface DateContextProps {
   date: dayjs.Dayjs;
   setDate: (date: dayjs.Dayjs) => void;
   today: dayjs.Dayjs;
   getCurrentMonth: () => number;
+  toKSTISOString: (date: dayjs.Dayjs) => string;
 }
 
 const DateContext = createContext<DateContextProps | undefined>(undefined);
 
 export function DateProvider({ children }: { children: ReactNode }) {
-  const today = dayjs(); // 오늘 날짜
-  const [date, setDate] = useState<dayjs.Dayjs>(today); // 현재 날짜 상태
+  const today = dayjs(); // 초기값, 오늘 날짜
+  const [date, setDate] = useState<dayjs.Dayjs>(today); // 선택한 날짜
 
   dayjs.locale('ko');
 
@@ -23,8 +29,15 @@ export function DateProvider({ children }: { children: ReactNode }) {
     return date.month() + 1;
   };
 
+  // 한국 시간대 ISO 변환
+  const toKSTISOString = (date: dayjs.Dayjs) => {
+    return dayjs(date).tz('Asia/Seoul').format();
+  };
+
   return (
-    <DateContext.Provider value={{ date, setDate, today, getCurrentMonth }}>
+    <DateContext.Provider
+      value={{ date, setDate, today, getCurrentMonth, toKSTISOString }}
+    >
       {children}
     </DateContext.Provider>
   );
