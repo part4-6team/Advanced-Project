a6a508fa6969bc4b51594cefcd7ce807343efb6d
Choi Young Sun <sunn7931@naver.com>
2024-10-29 21:18:41 +0900

    ZERO-135 [Design]: 로딩스피너 디자인 수정

--- src/components/@shared/LoadingSpinner.tsx
+++ src/components/@shared/LoadingSpinner.tsx
@@ -1,22 +1,18 @@
-import Image from 'next/image';
 import styles from '../../styles/carAnimation.module.css';
 
 export default function LoadingSpinner() {
   return (
-    <>
+    <div className="relative mt-[50px]">
+      <p className="text-center text-[30px] text-brand-tertiary">Loading...</p>
       <div
-        className={`${styles.carAnimation} relative mx-auto mt-[100px] h-[226px] w-[289px]`}
+        className={`${styles.carAnimation} relative mx-auto mt-[100px] h-[240px] w-[289px]`}
       >
-        <Image
+        <img
           src="/images/loading1.png"
           alt="기차 타는 아저씨"
-          fill
           className={styles.carImage}
         />
       </div>
-      <p className="mt-[50%] text-center text-[30px] text-brand-tertiary">
-        Loading...
-      </p>
-    </>
+    </div>
   );
 }
