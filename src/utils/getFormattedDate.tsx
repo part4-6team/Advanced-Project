264d160abae7d7c584405b093aaaa9e55592006c
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-10-29 18:30:55 +0900

    ZERO-120 [Feat]: 날짜 포맷팅 함수 추가

--- src/utils/getFormattedDate.tsx
+++ src/utils/getFormattedDate.tsx
@@ -4,9 +4,17 @@ import 'dayjs/locale/ko';
 // 한국어 로케일
 dayjs.locale('ko');
 
-const getFormattedDate = (date: dayjs.Dayjs | string): string => {
+export const formatTaskListDate = (date: dayjs.Dayjs | string): string => {
   const dayjsDate = typeof date === 'string' ? dayjs(date) : date;
   return dayjsDate.format('M월 D일 (ddd)');
 };
 
-export default getFormattedDate;
+export const formatTaskCardDate = (date: dayjs.Dayjs | string): string => {
+  const dayjsDate = typeof date === 'string' ? dayjs(date) : date;
+  return dayjsDate.format('YYYY년 MM월 DD일');
+};
+
+export const formatTaskWriterDate = (date: dayjs.Dayjs | string): string => {
+  const dayjsDate = typeof date === 'string' ? dayjs(date) : date;
+  return dayjsDate.format('YYYY.MM.DD');
+};
