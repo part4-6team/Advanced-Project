54a3a9af0f6070d845e3894c98ec757fcb6efdbf
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-11-04 04:03:43 +0900

    ZERO-161 [Feat]: taskList의 페이지네이션 추가

--- src/components/tasks/Layout/TaskList.tsx
+++ src/components/tasks/Layout/TaskList.tsx
@@ -1,7 +1,10 @@
 import Link from 'next/link';
-import { useEffect, useCallback } from 'react';
+import Image from 'next/image';
+
+import { useEffect, useCallback, useState, useRef } from 'react';
 import { useRouter } from 'next/router';
 import { useTaskListStore } from '@/src/stores/taskListStore';
+import ICON_PATHS from '@constants/iconPaths';
 import TaskCard from '../TaskCard';
 
 export default function TaskList() {
@@ -9,6 +12,10 @@ export default function TaskList() {
   const { query } = router;
   const { teamid, taskListId } = query;
   const { taskLists, setTaskListId, tasks, setTasks } = useTaskListStore();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(0);
+  const ulRef = useRef<HTMLUListElement | null>(null);
 
   // 선택된 taskList의 tasks[]를 가져오는 함수
   const fetchTasks = useCallback(
@@ -34,18 +41,86 @@ export default function TaskList() {
     setTasks(tasks);
   }, [tasks, setTasks]);
 
+  // 화면 크기에 따라 li 너비 결정
+  const getItemWidth = () => {
+    const width = window.innerWidth;
+    if (width < 768) {
+      return 150; // 모바일
+    }
+    if (width < 1240) {
+      return 200; // 태블릿
+    }
+    return 250; // PC
+  };
+
+  // 페이지당 항목 수 계산
+  const updateItemsPerPage = useCallback(() => {
+    if (ulRef.current) {
+      const containerWidth = ulRef.current.getBoundingClientRect().width;
+      const itemWidth = getItemWidth();
+      const newItemsPerPage = Math.floor(containerWidth / itemWidth);
+      setItemsPerPage(newItemsPerPage);
+      setCurrentPage((prevPage) =>
+        Math.min(prevPage, Math.ceil(taskLists.length / newItemsPerPage))
+      );
+    }
+  }, [taskLists.length]);
+
+  // ResizeObserver를 사용하여 넓이 측정
+  useEffect(() => {
+    const observer = new ResizeObserver(() => {
+      updateItemsPerPage();
+    });
+
+    const currentUlRef = ulRef.current;
+    if (currentUlRef) {
+      observer.observe(currentUlRef);
+    }
+
+    return () => {
+      if (currentUlRef) {
+        observer.unobserve(currentUlRef);
+      }
+    };
+  }, [updateItemsPerPage]);
+
+  // 페이지 변경 시 재계산
+  useEffect(() => {
+    updateItemsPerPage();
+  }, [currentPage, updateItemsPerPage]);
+
+  const totalPages = Math.ceil(taskLists.length / itemsPerPage);
+
+  // 남은 배열 슬라이스
+  const sliceTaskLists = taskLists.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
   return (
     <section className="flex flex-col gap-4">
-      <ul className="flex gap-3">
-        {taskLists.map((taskList) => (
+      <ul ref={ulRef} className="flex w-full justify-around gap-3">
+        {sliceTaskLists.map((taskList) => (
           <li
             key={taskList.id}
-            className={
+            className={`max-w-[250px] overflow-hidden text-ellipsis whitespace-nowrap text-center  ${
               Number(taskListId) === taskList.id ||
               (taskListId === undefined && Number(teamid) === taskList.id)
                 ? 'border-b-[1px] border-b-white text-white'
                 : 'text-text-default'
-            }
+            }`}
           >
             <Link
               href={{
@@ -60,6 +135,44 @@ export default function TaskList() {
           </li>
         ))}
       </ul>
+      <div className="mb-4 flex justify-between">
+        <button
+          type="button"
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+          className={`flex items-center gap-1 text-md-regular ${
+            currentPage === 1 ? 'text-text-default' : 'text-brand-primary'
+          }`}
+        >
+          <Image
+            alt="왼쪽 화살표 아이콘"
+            src={ICON_PATHS.ARROW_LEFT}
+            width={12}
+            height={12}
+            className="mx-auto"
+          />
+          <span>이전 목록</span>
+        </button>
+        <button
+          type="button"
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+          className={`flex items-center gap-1 text-md-regular ${
+            currentPage === totalPages
+              ? 'text-text-default'
+              : 'text-brand-primary'
+          }`}
+        >
+          <span>다음 목록</span>
+          <Image
+            alt="오른쪽 화살표 아이콘"
+            src={ICON_PATHS.ARROW_RIGHT}
+            width={12}
+            height={12}
+            className="mx-auto"
+          />
+        </button>
+      </div>
       {tasks.length > 0 ? (
         <ul className="flex flex-col gap-4">
           {tasks.length > 0 &&
