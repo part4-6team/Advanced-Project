cc8bb416662ebec07dcd9e108090685ffecf6689
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-11-06 14:59:49 +0900

    ZERO-100 [Design]: 문자열 버튼 애니메이션, disabled 상태 추가

--- src/components/@shared/animation/TextButtonMotion.tsx
+++ src/components/@shared/animation/TextButtonMotion.tsx
@@ -4,19 +4,20 @@ import { ReactNode } from 'react';
 interface TextButtonMotionProps {
   children: ReactNode;
   className?: string;
+  disabled?: boolean;
 }
 export default function TextButtonMotion({
   children,
   className,
+  disabled = false,
 }: TextButtonMotionProps) {
   return (
     <motion.div
-      whileHover={{
-        WebkitBackgroundClip: 'text',
-        scale: 1.05,
-      }}
+      whileHover={
+        !disabled ? { WebkitBackgroundClip: 'text', scale: 1.05 } : {}
+      }
       transition={{ duration: 0.2 }}
-      className={className}
+      className={`${className} ${disabled ? 'cursor-not-allowed opacity-30' : ''}`}
     >
       {children}
     </motion.div>
