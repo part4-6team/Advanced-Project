739d84cb24f5e325cdad1454feb9afa81b4d3654
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-11-07 16:27:13 +0900

    ZERO-165 [Feat]: 오늘 기준 이전 날짜에서 할 일 추가 버튼 disabled 처리

--- src/components/tasks/UI/button/AddTaskButton.tsx
+++ src/components/tasks/UI/button/AddTaskButton.tsx
@@ -1,12 +1,16 @@
 import { useModal } from '@hooks/useModal';
 import { useTaskListStore } from '@/src/stores/taskListStore';
+import { useDate } from '@/src/contexts/DateContext';
+import ClickMotion from '@components/@shared/animation/ClickMotion';
 import Button from '@components/@shared/Button';
 import AddTaskModal from '../modal/AddTaskModal';
-import ClickMotion from '@components/@shared/animation/ClickMotion';
 
 export default function AddTaskButton() {
+  const { date, today } = useDate();
   const { isOpen, onOpen, onClose } = useModal();
   const { isSidebarOpen, tasks } = useTaskListStore();
+
+  const isDisabled = date.isBefore(today, 'day');
 
   return (
     <>
@@ -14,7 +18,12 @@ export default function AddTaskButton() {
         <ClickMotion
           className={`${tasks.length > 0 ? 'fixed bottom-8 right-8 xl:bottom-12' : 'flex justify-center'}`}
         >
-          <Button bgColor="green" shape="round" onClick={onOpen}>
+          <Button
+            bgColor="green"
+            shape="round"
+            onClick={onOpen}
+            disabled={isDisabled}
+          >
             + 할 일 추가
           </Button>
         </ClickMotion>
