75c748d14d9a1cd92a081fff03795fc5fea57b80
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-11-07 16:13:09 +0900

    ZERO-165 [Design]: 애니메이션 적용

--- src/components/tasks/UI/button/AddTaskButton.tsx
+++ src/components/tasks/UI/button/AddTaskButton.tsx
@@ -2,6 +2,7 @@ import { useModal } from '@hooks/useModal';
 import { useTaskListStore } from '@/src/stores/taskListStore';
 import Button from '@components/@shared/Button';
 import AddTaskModal from '../modal/AddTaskModal';
+import ClickMotion from '@components/@shared/animation/ClickMotion';
 
 export default function AddTaskButton() {
   const { isOpen, onOpen, onClose } = useModal();
@@ -10,13 +11,13 @@ export default function AddTaskButton() {
   return (
     <>
       {!isSidebarOpen && (
-        <div
+        <ClickMotion
           className={`${tasks.length > 0 ? 'fixed bottom-8 right-8 xl:bottom-12' : 'flex justify-center'}`}
         >
           <Button bgColor="green" shape="round" onClick={onOpen}>
             + 할 일 추가
           </Button>
-        </div>
+        </ClickMotion>
       )}
 
       {isOpen && <AddTaskModal isOpen={isOpen} onClose={onClose} />}
