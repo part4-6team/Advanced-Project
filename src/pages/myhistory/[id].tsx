aa1e4bd74164b4e3c6de8ac9c495a9b4aa2d2d36
jangyonghan <wkddydgks1@gmail.com>
2024-11-08 00:43:23 +0900

    ZERO-174 [feat] : 할일완료 그래프 추가

--- src/pages/myhistory/[id].tsx
+++ src/pages/myhistory/[id].tsx
@@ -1,12 +1,20 @@
+import Contributions from '@components/myhistory/Contributions';
 import MyTask from '@components/myhistory/MyTask';
 
 export default function Myhistory() {
   return (
     <main className="mx-auto mt-8 w-auto max-w-[1200px] text-text-primary">
-      <h1 className="mb-[27px] ml-4 text-2lg-bold ">마이 히스토리</h1>
-      <section className="mx-4 mb-10">
-        <MyTask />
-      </section>
+      <div className="flex flex-col-reverse md:flex-row ">
+        <div className="mx-10 md:w-[800px]">
+          <h1 className="mb-[27px] ml-4 text-2xl-bold ">마이 히스토리</h1>
+          <section className="mb-10 mt-10">
+            <MyTask />
+          </section>
+        </div>
+        <div className="mb-10 mt-[67px]">
+          <Contributions />
+        </div>
+      </div>
     </main>
   );
 }
