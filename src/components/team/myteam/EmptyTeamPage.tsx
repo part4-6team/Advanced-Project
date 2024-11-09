5c952f98aad4d7f5f42c26de66282306c0a3dd92
Choi Young Sun <sunn7931@naver.com>
2024-11-09 13:07:44 +0900

    ZERO-178 [Design]: 팀 소속 없는 이미지 변경

--- src/components/team/myteam/EmptyTeamPage.tsx
+++ src/components/team/myteam/EmptyTeamPage.tsx
@@ -6,12 +6,12 @@ export default function EmptyTeamPage() {
     <main className="mx-auto mt-[100px] flex h-auto w-full min-w-[300px] flex-col items-center px-[31px] text-center text-md-medium text-text-default md:px-[112px] xl:w-[1200px] xl:text-lg-medium">
       <div className="relative mx-auto flex h-auto w-full flex-grow xl:block xl:h-[255px] xl:w-[810px]">
         <img
-          src="/images/team_none.png"
+          src="/images/team_none_tb.png"
           alt="PC 팀 소속 없는 이미지"
           style={{ objectFit: 'contain' }}
         />
       </div>
-      <p className="mt-[48px]">아직 소속된 팀이 없습니다.</p>
+      <p className="mt-[70px] xl:mt-[120px]">아직 소속된 팀이 없습니다.</p>
       <p className="mt-[4px]">팀을 생성하거나 팀에 참여해보세요.</p>
       <Link href="/addteam">
         <Button width={136} height={48} fontSize="14" className="mt-[48px]">
